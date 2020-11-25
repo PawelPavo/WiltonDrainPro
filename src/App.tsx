b26614d32138678e46203976d0312ee33b8eefe8
PawelPavo <Pawelebllc@gmail.com>
2020-11-25 17:53:32 -0500

    deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavigationBar from './components/NavBar';
 import Contact from './views/Contact';
 import Home from './views/Home';
@@ -8,23 +8,26 @@ import Services from './views/Services';
 function App() {
   return (
     <>
-          <div className="bg-light">
-      <BrowserRouter>
-        <NavigationBar / >
+      <HashRouter basename='/'>
+        <div className="bg-light">
+          <BrowserRouter>
+            <NavigationBar />
 
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/services">
-            <Services />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-          </div>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/services">
+                <Services />
+              </Route>
+              <Route exact path="/contact">
+                <Contact />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </HashRouter>
+
     </>
   );
 }
